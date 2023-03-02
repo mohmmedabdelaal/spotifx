9592af5deecfb93bd29c585f8b562fa030204a27
mohammedabdelaal <medo.elaal@gmail.com>
2023-03-02 03:03:09 +0200

    serveless Api created

---
+++ pages/api/signup.ts
@@ -0,0 +1,9 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+import cookie from 'cookie';
+import {PrismaClient} from "@prisma/client";
+import {NextApiRequest, NextApiResponse} from 'next'
+
+export default async (req: NextApiRequest,res: NextApiResponse) =>{
+
+}
---
+++ prisma/Seed.ts
@@ -0,0 +1,64 @@
+import {PrismaClient} from '@prisma/client'
+import bcrypt from 'bcrypt'
+import {artistsData} from "./SongsData";
+import {Prosto_One} from "@next/font/google";
+
+const prisma = new PrismaClient();
+
+const run = async () => {
+    await  Promise.all( artistsData.map(async (artist) =>{
+        await prisma.artist.upsert({
+            where: {name: artist.name},
+            update: {},
+            create: {
+                name: artist.name,
+                songs: {
+                    create: artist.songs.map( (song) =>({
+                        name:song.name,
+                        duration: song.duration,
+                        url: song.url
+                    }))
+                }
+            }
+        } )
+    }))
+
+    const salt = bcrypt.genSaltSync();
+    const user = await prisma.user.upsert({
+        where: { email: "user@test.com" },
+        update: {},
+        create: {
+            email: "user@test.com",
+            password: bcrypt.hashSync("password", salt),
+            firstname: "Mohamed",
+            lastname: "Abdelaal",
+        },
+    });
+
+    const songs = await prisma.song.findMany({});
+
+    await Promise.all(new Array(20).fill(1).map(async (_,i) => {
+        return prisma.playlist.create({
+           data: {
+               name: `Playlist @${i+1}`,
+               user: {
+                   connect: {id : user.id}
+               },
+               songs: {
+                   connect: songs.map((song) => ({
+                       id: song.id
+                   }))
+               }
+            }
+        })
+    }))
+
+}
+
+
+run().catch((e) =>{
+    console.error(e);
+    process.exit(1);
+}).finally(async () =>{
+    await prisma.$disconnect();
+})
